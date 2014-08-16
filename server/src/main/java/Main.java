13bfa81c5a82ab732c132591fcb97d537472247e
IlyaMakeev <ilya.makeev@gmail.com>
2014-08-17 00:09:51 +0400

    some

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -13,11 +13,11 @@ public class Main {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sbb_unit");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        System.out.println("\n get passenger by train number:\n-------------");
-        List<Passenger> passengerList = getPassengerByTrain(123, entityManager);
-        for (Passenger passenger : passengerList) {
-            System.out.println(passenger);
-        }
+//        System.out.println("\n get passenger by train number:\n-------------");
+//        List<Passenger> passengerList = getPassengerByTrain(123, entityManager);
+//        for (Passenger passenger : passengerList) {
+//            System.out.println(passenger);
+//        }
 
         System.out.println("\n print station shedule:\n-------------");
         printStationSchedule("Moskow", entityManager);
@@ -26,35 +26,79 @@ public class Main {
         entityManagerFactory.close();
     }
 
-    static void printStationSchedule(String stationName, EntityManager entityManager) {
-        Query query = entityManager.createQuery("SELECT st FROM Station st WHERE st.name=:stName");
-        query.setParameter("stName", stationName);
-        List<Station> list = query.getResultList();
-        if (list.isEmpty()) {
-            log.warning("station not found");
-        } else {
-            Station station = list.get(0);
-            station.printSchedule();
-        }
-    }
-
     static List<Passenger> getPassengerByTrain(int trainNum, EntityManager entityManager) {
-        Query query = entityManager.createQuery("SELECT tr FROM Train tr WHERE tr.number=:numb");
+        Query query = entityManager.createQuery("SELECT ts.passenger FROM Train tr join tr.ticketList ts where tr.number =:numb");
         query.setParameter("numb", trainNum);
-        List<Train> list = query.getResultList();
-        List<Passenger> passengerList = new ArrayList<Passenger>();
-        if (list.isEmpty()) {
-            log.warning("train not found");
-            return passengerList;
-        } else {
-            List<Ticket> ticketList = list.get(0).getTicketList();
-            for (Ticket ticket : ticketList) {
-                passengerList.add(ticket.getPassenger());
-            }
-            return passengerList;
-        }
-
+        List<Passenger> passengerList = query.getResultList();
+        return passengerList;
     }
+
+    static void printStationSchedule(String stationName, EntityManager entityManager) {
+        Query query = entityManager.createQuery("SELECT ts.train FROM Station st join st.scheduleList ts where st.name =:stName");
+        query.setParameter("stName", stationName);
+        List<Train> list = query.getResultList();
+        for (Train train:list){
+            System.out.println(train);
+        }
+        //return passengerList;
+    }
+
+//    static void printStationSchedule1(String stationName, EntityManager entityManager) {
+//        Query query = entityManager.createQuery("SELECT st FROM Station st WHERE st.name=:stName");
+//        query.setParameter("stName", stationName);
+//        List<Station> list = query.getResultList();
+//        if (list.isEmpty()) {
+//            log.warning("station not found");
+//        } else {
+//            Station station = list.get(0);
+//            station.printSchedule();
+//        }
+//    }
+
+//    static List<Passenger> getPassengerByTrain(int trainNum, EntityManager entityManager) {
+//        Query query = entityManager.createQuery("SELECT tr FROM Train tr WHERE tr.number=:numb");
+//        query.setParameter("numb", trainNum);
+//        List<Train> list = query.getResultList();
+//        List<Passenger> passengerList = new ArrayList<Passenger>();
+//        if (list.isEmpty()) {
+//            log.warning("train not found");
+//            return passengerList;
+//        } else {
+//            List<Ticket> ticketList = list.get(0).getTicketList();
+//            for (Ticket ticket : ticketList) {
+//                passengerList.add(ticket.getPassenger());
+//            }
+//            return passengerList;
+//        }
+//
+//    }
+//
+//    static List<Passenger> getPassengerByTrain(int trainNum, EntityManager entityManager) {
+//        //
+//        Query query = entityManager.createQuery("SELECT ts.passenger FROM Train tr join tr.ticketList ts where tr.number =:numb");
+//       // Query query = entityManager.createQuery("SELECT trn.ticketList FROM (SELECT Train tr FROM where tr.number =:numb) trn  ");
+//        query.setParameter("numb", trainNum);
+//        //Query query = entityManager.createQuery("SELECT tr.ticketList FROM Train tr");
+//       // List<Train> list = query.getResultList();
+////        List<Ticket> list = query.getResultList();
+////        for (Ticket t :list){
+////            System.out.println(t);
+////        }
+//        List<Passenger> passengerList = query.getResultList();
+////        if (list.isEmpty()) {
+////            log.warning("train not found");
+////            return passengerList;
+////        } else {
+////            List<Ticket> ticketList = list.get(0).getTicketList();
+////            for (Ticket ticket : ticketList) {
+////                passengerList.add(ticket.getPassenger());
+////            }
+////            return passengerList;
+////        }
+//        return passengerList;
+//
+//    }
+
 
 
 }
